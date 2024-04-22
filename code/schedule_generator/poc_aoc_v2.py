6a242f9b86b189698e5a2cf5e989361f6f1076fe
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-04-22 18:20:44 +0200

    Adds la16 benchmark

--- code/schedule_generator/poc_aoc_v2.py
+++ code/schedule_generator/poc_aoc_v2.py
@@ -342,16 +342,29 @@ if __name__ == "__main__":
 1 5 0 5 2 5 3 3 4 8 5 9
 2 9 1 3 4 5 5 4 0 3 3 1
 1 3 3 3 5 9 0 10 4 4 2 1"""
-    problem = JobShopProblem.from_standard_specification(la01_text)
+    la16_text = """1 21 6 71 9 16 8 52 7 26 2 34 0 53 4 21 3 55 5 95
+4 55 2 31 5 98 9 79 0 12 7 66 1	42	8	77	6	77	3	39
+3	34	2	64	8	62	1	19	4	92	9	79	7	43	6	54	0	83	5	37
+1	87	3	69	2	87	7	38	8	24	9	83	6	41	0	93	5	77	4	60
+2	98	0	44	5	25	6	75	7	43	1	49	4	96	9	77	3	17	8	79
+2	35	3	76	5	28	9	10	4	61	6	9	0	95	8	35	1	7	7	95
+3	16	2	59	0	46	1	91	9	43	8	50	6	52	5	59	4	28	7	27
+1	45	0	87	3	41	4	20	6	54	9	43	8	14	5	9	2	39	7	71
+4	33	2	37	8	66	5	33	3	26	7	8	1	28	6	89	9	42	0	78
+8	69	9	81	2	94	4	96	3	27	0	69	7	45	6	78	1	74	5	84
+
+"""
+    # problem = JobShopProblem.from_standard_specification(la01_text)
     # problem = JobShopProblem.from_standard_specification(ft06_text)
+    problem = JobShopProblem.from_standard_specification(la16_text)
     # problem = JobShopProblem.from_list([[(0, 3, None), (1, 2, None), (2, 2, None)], [(0, 2, None), (2, 1, None), (1, 4, None)], [(1, 4, None), (2, 3, None)]])
 
     start_time = time.time()
 
-    aco = ACO(problem, n_ants=25, n_iter=1000, verbose=False, seed=9525, beta=1)
+    aco = ACO(problem, n_ants=50, n_iter=4000, verbose=False, seed=2345005, beta=1)
     aco.run()
     print(
-        f"Best makespan found: {aco.best_solution[0]}, which is {(aco.best_solution[0] - 660.0) / 666.0:.2%} of the optimal solution for this problem."
+        f"Best makespan found: {aco.best_solution[0]}, which is {(aco.best_solution[0] - 945.0) / 945.0:.2%} of the optimal solution for this problem."
     )
     print(f"Path: {aco.best_solution[1]}")
     print(f"Took {time.time() - start_time:.2f} seconds to run.")
