ea794a0cc0c72d533254e30e2c2716c949568919
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-05-15 14:51:51 +0200

    Adds make_schedule function

--- src/stable_schedule_generator/main.py
+++ src/stable_schedule_generator/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from pydantic import BaseModel
 from src.production_orders import Data, parse_data, Product, BillOfMaterial
 
+DAY_MINUTES = 24 * 60
 
 class Job(BaseModel):
     available_machines: list[int]
@@ -16,11 +17,13 @@ class Job(BaseModel):
 class Machine(BaseModel):
     name: str
     machine_id: int
-    start_time: int
+    start_time: int = 0
     end_time: int
     allow_preemption: bool = False
     max_units_per_run: int = 1
+    minutes_per_run: float
 
+class ScheduleError(Exception):...
 
 class JobShopProblem:
     def __init__(self, data: Data, jobs: list[Job], machines: list[Machine]) -> None:
@@ -48,6 +51,7 @@ class JobShopProblem:
                 end_time=m.stops_at.hour * 60 + m.stops_at.minute,
                 allow_preemption=(m.name.lower().startswith("bottling")),
                 max_units_per_run=m.max_units_per_run,
+                minutes_per_run=m.minutes_per_run,
             )
             for idx, m in enumerate(data.workstations)
         ]
@@ -131,7 +135,95 @@ class JobShopProblem:
                             amount=amount,
                         )
                     )
-        return cls(data=data, jobs=sub_jobs, machines=machines)
+        jssp = cls(data=data, jobs=sub_jobs, machines=machines)
+
+        # Set setup-times
+        for j1_idx, j1 in enumerate(jssp.jobs):
+            for j2_idx, j2 in enumerate(jssp.jobs):
+                if j1 == j2 or j1.production_order_nr == j2.production_order_nr:
+                    continue
+                if j1.station_settings["taste"] != j2.station_settings["taste"]:
+                    jssp.setup_times[j1_idx, j2_idx] += jssp.data.workstations[j1.available_machines[0]].minutes_changeover_time_taste
+
+                if j1.station_settings["bottle_size"] != j2.station_settings["bottle_size"]:
+                    jssp.setup_times[j1_idx, j2_idx] += jssp.data.workstations[j1.available_machines[0]].minutes_changeover_time_bottle_size
+
+        return jssp
+
+    def make_schedule(self, job_order: list[int], machine_assignment: list[int]):
+        """Create a schedule based on a give job order and machine assignment.
+
+        Note that the job_order is relative, and machine_assignment is absolute. That means that
+        the machine_assignment have at index i the machine assignment for job i. While the job_order
+        at index i is the job that should be done at position i in relation to the other jobs in the list.
+
+        Args:
+            job_order (list[int]): the order of the jobs that should be done
+            machine_assignment (list[int]): what machine job i should be done on
+
+        Raises:
+            ScheduleError: raised if the job order or machine assignment is incorrect
+
+        Returns:
+            dict[int, list[tuple[int, int, int]]]: the schedule for each machine with the job id, start time and end time
+                relative to midnight of day 0.
+        """
+        # Contains the schedule for each machine
+        schedule: dict[int, list[tuple[int, int, int]]] = {
+            m.machine_id: [(-1, 0, m.start_time)] for m in self.machines
+        }
+
+        # Contains the machine, start time and end time for each job
+        job_schedule: dict[int, tuple[int, int, int]] = dict()
+
+        for task_idx in job_order:
+            task: Job = self.jobs[task_idx]
+            print(f"{task_idx=}, {task=}")
+            machine_idx = machine_assignment[task_idx]
+            if machine_idx not in task.available_machines:
+                raise ScheduleError(f"Machine {machine_idx} not available for task {task_idx}")
+            machine = self.machines[machine_idx]
+
+            relevant_task: list[tuple[int, int, int]] = list()
+
+            # Get the last job on the same machine
+            latest_job_on_same_machine = schedule[machine_idx][-1]
+            relevant_task.append(latest_job_on_same_machine)
+
+            # Check for dependencies
+            if len(task.dependencies) > 0:
+                for dep in task.dependencies:
+                    if dep_task := job_schedule.get(dep, None):
+                        relevant_task.append(dep_task)
+                    else:
+                        raise ScheduleError(f"Dependency {dep} not scheduled before {task_idx}")
+                
+            # Get the start time of the task
+            start_time = max([task[2] for task in relevant_task])
+
+            task_duration: int = int(
+                machine.minutes_per_run * task.amount if self.data.workstations[machine_idx].max_units_per_run == 1 else machine.minutes_per_run
+                + self.setup_times[latest_job_on_same_machine[0], task_idx]
+            )
+            # If task is schedule before the machine starts, we move it to the start time
+            if start_time % DAY_MINUTES < machine.start_time:
+                start_time = machine.start_time + (start_time // DAY_MINUTES) * DAY_MINUTES
+
+            # If the task ends after the machine stops, we move it to the next day, unless we allow preemption.
+            # If we allow preemption we will just continue with the work the next day
+            if start_time % DAY_MINUTES + task_duration > machine.end_time:
+                # If we allow for preemption we will just add to the duration the time inbetween start and end time
+                if machine.allow_preemption:
+                    task_duration += DAY_MINUTES - machine.end_time + machine.start_time
+                else:
+                    start_time = machine.start_time + (start_time // DAY_MINUTES + 1) * DAY_MINUTES
+
+            end_time = start_time + task_duration
+            schedule[machine_idx].append((task_idx, start_time, end_time))
+            job_schedule[task_idx] = (machine_idx, start_time, end_time)
+
+        return schedule
+
 
 
 class ObjectiveFunction(Enum):
@@ -142,5 +234,7 @@ class ObjectiveFunction(Enum):
 
 
 if __name__ == "__main__":
-    data = parse_data("examples/data_v1_single.xlsx")
+    data = parse_data("examples/data_v1.xlsx")
     jssp = JobShopProblem.from_data(data)
+    print(jssp.jobs[:3])
+    print(jssp.make_schedule([0,2,1], [0, 1, 0]))
