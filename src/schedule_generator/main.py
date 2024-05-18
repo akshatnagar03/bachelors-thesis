!Important! Fixes data being generated wrongly with batch calculation

src/schedule_generator/main.py
@@ -146,7 +146,7 @@ class JobShopProblem:
         # plt.xticks(ticks=np.concatenate([day_markers]), labels=day_labels)
         plt.yticks(
             ticks=np.arange(1, len(schedule) + 1),
-            labels=[f"Machine {m}" for m in schedule.keys()],
+            labels=[str(self.machines[m].name) for m in schedule.keys()],
         )
         plt.xlabel("Days")
         plt.ylabel("Machine")
@@ -267,7 +267,7 @@ class JobShopProblem:
                         amount = prod["amount"] % ((batch_info["batches"] - 1) * amount)
                         if amount == 0:
                             amount = prod["amount"] // batch_info["batches"]
-
+                    amount = np.ceil(amount)
                     sub_jobs.append(
                         Job(
                             available_machines={

src/stable_schedule_generator/main.py
@@ -10,7 +10,7 @@ DAY_MINUTES = 24 * 60
 
 
 class Job(BaseModel):
-    available_machines: dict[int, int]
+    available_machines: dict[int, float]
     dependencies: list[int]
     production_order_nr: str
     station_settings: dict[str, Any] = dict()
@@ -234,7 +234,7 @@ class JobShopProblem:
                 min_max_units_per_run = min(
                     [machines[m].max_units_per_run for m in prod_info["machines"]]
                 )
-                prod_info["batches"] = max(int(amount // min_max_units_per_run), 1)
+                prod_info["batches"] = int(np.ceil(amount / min_max_units_per_run))
                 prod_info["batches_amount"] = min_max_units_per_run
                 remainder = int(amount % min_max_units_per_run)
                 prod_info["batches_remainder"] = (
@@ -256,11 +256,11 @@ class JobShopProblem:
                         dependencies.append(len(sub_jobs) - 1)
 
                     amount = prod["amount"] // batch_info["batches"]
-                    if i == batch_info["batches"] - 1:
-                        amount = prod["amount"] % batch_info["batches"]
+                    if i == batch_info["batches"] - 1 and batch_info["batches"] > 1:
+                        amount = prod["amount"] % ((batch_info["batches"] - 1) * amount)
                         if amount == 0:
                             amount = prod["amount"] // batch_info["batches"]
-
+                    amount = np.ceil(amount)
                     sub_jobs.append(
                         Job(
                             available_machines={