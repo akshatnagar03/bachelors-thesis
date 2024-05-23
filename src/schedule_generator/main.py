1bc553d679bf1c781dee7119d4f7c193f14d711f
Albin Lindqvist <albin.lindqvist@hotmail.se>
2024-05-23 17:33:32 +0200

    Found new best lower bounds

--- src/schedule_generator/main.py
+++ src/schedule_generator/main.py
@@ -641,9 +641,9 @@ class JobShopProblem:
         if self.LOW_TARDINESS is None:
             self.LOW_TARDINESS = 16.0
         if self.LOW_TOTAL_SETUP_TIME is None:
-            self.LOW_TOTAL_SETUP_TIME = 145.0
+            self.LOW_TOTAL_SETUP_TIME = 110.0
         if self.LOW_MAKESPAN is None:
-            self.LOW_MAKESPAN = 3600.0
+            self.LOW_MAKESPAN = 3510.0
 
         return (
             (tardiness - self.LOW_TARDINESS) / self.LOW_TARDINESS
